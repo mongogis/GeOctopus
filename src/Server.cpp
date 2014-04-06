cdbc3b408ac0504378c57c9219048068b13eb3c2
Hanson <songhuadan@foxmail.com>
2014-04-05 23:20:17 -0500

    implement2

--- src/Server.cpp
+++ src/Server.cpp
@@ -10,33 +10,26 @@ hpgc::IAlgorithmFactory * hpgc::Server::Create(int argc, char ** argv)
 	if(m_instance =NULL)
 		m_instance=new ServiceCenter();
 
-	char * pszAlg;
+	char * pszAlg=NULL;
 	IAlgorithmFactory * pfactory=NULL;
 
 	for ( int i = 1; i < argc; i++ )
-	{
 		if ( EQUAL(argv[i], "-alg") && i < argc - 1 )
-		{
 			pszAlg = argv[++i];
-		}
-		else
-			return 0;
-	}
+		
+	if(pszAlg==NULL)
+		return NULL;
 
 	for(std::list<IAlgorithmFactory *>::iterator i=m_instance->m_factory_list.begin();i!=m_instance->m_factory_list.end();i++)
-	{
 		if (EQUAL((*i)->GetName.c_str(),pszAlg))
-		{
 			return *i;
-		}
-	}
-	
+		
 	return 0;
 }
 
 hpgc::Server::ServiceCenter()
 {
-	m_factory_list.push_back(NULL);
+	//m_factory_list.push_back(NULL);
 }
 
 bool hpgc::Server::AddFactory(IAlgorithmFactory * pAlgFactory)
@@ -47,10 +40,6 @@ bool hpgc::Server::AddFactory(IAlgorithmFactory * pAlgFactory)
 bool hpgc::Server::RemoveFactory(char * pFactoryName)
 {
 	for(std::list<IAlgorithmFactory *>::iterator i=m_factory_list.begin();i!=m_factory_list.end();i++)
-	{
 		if (EQUAL((*i)->GetName.c_str(),pFactoryName))
-		{
 			m_factory_list.remove(*i);
-		}
-	}
 }
