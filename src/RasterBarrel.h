5e21f24321db0a96ffe8389775c4c0311731cbc7
huangtao <ht.anglenx@gmail.com>
2014-04-07 09:49:54 +0800

    add raster

---
+++ src/RasterBarrel.h
@@ -0,0 +1,8 @@
+#ifndef HPGC_RASTER_BARREL_H_INCLUDE
+#define HPGC_RASTER_BARREL_H_INCLUDE
+
+namespace hpgc{
+
+}
+
+#endif 
---
+++ src/RasterCellar.h
@@ -0,0 +1,8 @@
+#ifndef HPGC_RASTER_CELLAR_H_INCLUDE
+#define HPGC_RASTER_CELLAR_H_INCLUDE
+
+namespace hpgc{
+
+}
+
+#endif 
