268a7dd99026f0b159f0f8f100681d1fe785fbd8
huangtao <ht.anglenx@gmail.com>
2014-04-12 07:39:26 +0800

    add M2sScheduler

---
+++ src/M2sScheduler.h
@@ -0,0 +1,17 @@
+#ifndef M2sScheduler_h__
+#define M2sScheduler_h__
+
+namespace hpgc{
+	class M2sScheduler
+	{
+	public:
+		M2sScheduler();
+		~M2sScheduler();
+
+	private:
+
+	};
+
+}
+
+#endif // M2sScheduler_h__
